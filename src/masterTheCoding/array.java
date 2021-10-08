53b8843b7d7a348786a6356589e3eaaf08e269d8
hadar-c <cohen.hdr@gmail.com>
2021-10-09 01:03:30 +0300

    merge func commit

--- src/masterTheCoding/array.java
+++ src/masterTheCoding/array.java
@@ -15,4 +15,50 @@ public class array {
             System.out.print(revStr[i]);
         System.out.println("\n");
 	}
+	
+	// This function merge sorted arrays
+	static void merge() {
+		System.out.print("Enter the number of numbers you want to store in the first array: ");  
+		int n = sc.nextInt();  
+		int[] firstArray = new int[n];  
+		System.out.println("Enter the elements of the first array in a sorted way: ");  
+		for(int i = 0; i < n; i++)  //reading array elements from the user   
+			firstArray[i]=sc.nextInt();  
+		  
+		System.out.print("Enter the number of numbers you want to store in the second array: ");  
+		int m = sc.nextInt();  
+		int[] secondArray = new int[m]; 
+		System.out.println("Enter the elements of the second array in a sorted way: ");  
+		for(int i = 0; i < m; i++)  //reading array elements from the user   
+			secondArray[i]=sc.nextInt();  
+		
+		int[] mergedArray = new int[n+m];
+		int firstPointer = 0, secondPointer = 0;
+		for(int i = 0; i < n+m; i++) {
+			if(firstPointer == n) {
+				mergedArray[i] = secondArray[secondPointer];
+				secondPointer++;
+			}
+			else if(secondPointer == m) {
+				mergedArray[i] = firstArray[firstPointer];
+				firstPointer++;
+			}
+			else if(firstArray[firstPointer] <= secondArray[secondPointer]) {
+				mergedArray[i] = firstArray[firstPointer];
+				firstPointer++;				
+			}
+			else {
+				mergedArray[i] = secondArray[secondPointer];
+				secondPointer++;
+			}
+		}
+		System.out.println("The sorted array: ");
+		for(int i = 0; i < n+m; i++) {
+			System.out.print(mergedArray[i]);
+			if(i != (n + m -1)) {
+				System.out.print(", ");
+			}
+		}
+        System.out.println("\n");
+	}
 }
--- src/masterTheCoding/mainClass.java
+++ src/masterTheCoding/mainClass.java
@@ -12,7 +12,8 @@ public class mainClass {
 		while (running) {
 			System.out.println("Actions menu:");
 			System.out.println("   0. Exit\n" +
-					"   1. Reverse string");
+					"   1. Reverse a string\n" +
+					"   2. Merge sorted arrays");
 		    System.out.print("\nChoose action:");
 
 		    int choice = sc.nextInt();  // Read user input
@@ -20,6 +21,8 @@ public class mainClass {
 		    	running = false;
 		    else if (choice ==1) 
 		    	array.reverse();
+		    else if (choice ==2)
+		    	array.merge();
 				
 			
 			
